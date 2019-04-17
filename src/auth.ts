0239a1e4bbd6195626c14ba2c700e7f4b7045480
Jonny Nabors <jnabors@ford.com>
2019-04-17 14:58:43 -0400

    Check expiration time of refresh token and authenticate accordingly
    Signed-off-by: Chris Tobolski <ctobolsk@ford.com>

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import * as jwt from 'jsonwebtoken'
 import { stringify } from 'qs'
 
 const client_id = process.env.CLIENT_ID
@@ -6,7 +7,12 @@ const client_secret = process.env.CLIENT_SECRET
 const realm = process.env.REALM
 const hostname = process.env.HOSTNAME
 const tokenUrl = `https://accounts.${hostname}/auth/realms/${realm}/protocol/openid-connect/token`
-let refreshToken: string
+let refreshToken: Token
+
+interface Token {
+  value: string
+  expiresAt: number
+}
 
 axios.interceptors.response.use(handleSuccessfulResponse, handleError)
 
@@ -27,8 +33,9 @@ export async function authenticate() {
     } = await axios.post(tokenUrl, stringify(body), config)
     updateCredentials(access_token, refresh_token)
     console.log('Successfully authenticated!')
+    return access_token
   } catch (error) {
-    console.log('failed result', error.response.status)
+    console.log('failed result', error)
   }
 }
 
@@ -42,18 +49,34 @@ export async function refreshTokens() {
     grant_type: 'refresh_token',
     client_id,
     client_secret,
-    refresh_token: refreshToken
+    refresh_token: refreshToken.value
   }
   try {
-    const {
-      data: { access_token, refresh_token }
-    } = await axios.post(tokenUrl, stringify(body), config)
-    updateCredentials(access_token, refresh_token)
-    return access_token
+    const currentTime = Date.now()
+    if (currentTime > refreshToken.expiresAt) {
+      return await authenticate()
+    }
+    return await refresh(body, config)
   } catch (error) {
     console.log('Refresh Error:', error)
     throw new Error('Failed to refresh token')
   }
+}
+
+async function refresh(
+  body: {
+    grant_type: string
+    client_id: string | undefined
+    client_secret: string | undefined
+    refresh_token: string
+  },
+  config: AxiosRequestConfig
+) {
+  const {
+    data: { access_token, refresh_token }
+  } = await axios.post(tokenUrl, stringify(body), config)
+  updateCredentials(access_token, refresh_token)
+  return access_token
 }
 
 async function handleError(error: any) {
@@ -61,8 +84,6 @@ async function handleError(error: any) {
   if (!error.response) {
     return Promise.reject(error)
   }
-
-  console.log('Error response status:', error.response.status)
 
   if (error.response.status === 401) {
     try {
@@ -84,7 +105,8 @@ async function handleError(error: any) {
 
 function updateCredentials(access_token: string, refresh_token: string) {
   axios.defaults.headers.Authorization = `Bearer ${access_token}`
-  refreshToken = refresh_token
+  const decoded = jwt.decode(refresh_token) as any
+  refreshToken = { value: refresh_token, expiresAt: decoded.exp * 1000 }
 }
 
 async function retry(originalRequest: any) {
