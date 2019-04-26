2a3ddd08ebdf2a0d574044820e7fc62532a7a276
Jonny Nabors <jnabors@ford.com>
2019-04-26 15:07:57 -0400

    First time login and registration creates and returns user
    Signed-off-by: Chris Tobolski <ctobolsk@ford.com>

---
+++ src/repository.ts
@@ -0,0 +1,24 @@
+import { Client } from 'pg'
+const client = new Client({
+  connectionString: process.env.DATABASE_URL
+})
+console.log(`attempting to connect to db at ${process.env.DATABASE_URL}`)
+
+client
+  .connect()
+  .then(() => {
+    console.log('Connected to DB')
+    console.log('Updating tables')
+    client.query(`CREATE TABLE IF NOT EXISTS users (
+    "id" SERIAL primary key,
+    "sub" varchar(30) NOT NULL,
+    "vin" varchar(17)
+  )`)
+  })
+  .catch(error => {
+    console.log('Failed to connect to DB', error)
+  })
+
+export default {
+  query: (query: string) => client.query(query)
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,11 @@
 import { Request, Response, Application } from 'express'
 import { vehicleDataForVin, oilLifeRemainingForVin } from './auService'
+import { decode } from 'jsonwebtoken'
+import db from './repository'
 const jwt = require('express-jwt')
 var jwksRsa = require('jwks-rsa')
 
 const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the signing keys provided by the JWKS endpoint.
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
@@ -14,7 +13,6 @@ const checkJwt = jwt({
     jwksUri: `${process.env.AUTH0_DOMAIN}.well-known/jwks.json`
   }),
 
-  // Validate the audience and the issuer.
   audience: process.env.AUTH0_AUDIENCE,
   issuer: process.env.AUTH0_DOMAIN,
   algorithms: ['RS256']
@@ -39,5 +37,38 @@ export class Routes {
         const data = await oilLifeRemainingForVin()
         res.send(data)
       })
+
+    interface AccessToken {
+      sub: string
+    }
+
+    app.route('/login').get(checkJwt, async (req: Request, res: Response) => {
+      const token = req.headers.authorization!
+      const { sub } = decode(token.replace('Bearer ', '')) as AccessToken
+      try {
+        const user = await getUser(sub)
+        res.send({ user })
+      } catch (error) {
+        res.status(500).send({ error })
+      }
+    })
+
+    async function getUser(sub: string) {
+      const result = await db.query(`SELECT * from USERS where sub = '${sub}'`)
+      if (result.rowCount > 0) {
+        return result.rows[0]
+      } else {
+        return await registerUser(sub)
+      }
+    }
+
+    async function registerUser(sub: string) {
+      const result = await db.query(
+        `INSERT INTO USERS (sub) values ('${sub}') RETURNING id, sub, VIN;`
+      )
+      return result.rows[0]
+    }
+
+    // endpoint to create user
   }
 }
